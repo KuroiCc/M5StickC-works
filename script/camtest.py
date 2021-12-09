2f2047bc5611ee639539692453bb7d63ad0fff08
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-12-09 17:35:33 +0900

    camtest

--- script/camtest.py
+++ script/camtest.py
@@ -1,19 +1,22 @@
-import numpy as np
 import cv2
+import sys
+
+try:
+    sys.argv[1]
+except IndexError:
+    print("ファイル名を入力してください")
+    exit(0)
 
 cap = cv2.VideoCapture(0)
 
-while (True):
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+ret, frame = cap.read()
+cv2.imshow('frame', frame)
 
-    key = cv2.waitKey(1)
+key = cv2.waitKey(1)
 
-    if key == ord('q'):
-        break
-    if key == ord('s'):
-        path = "photo.jpg"
-        cv2.imwrite(path, frame)
+path = "%s.jpg" % sys.argv[1]
+
+cv2.imwrite(path, frame)
 
 cap.release()
 cv2.destroyAllWindows()
